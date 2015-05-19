66dfd45ae99906dad557c600ee8ef792731bc2f5
laurencehammond <laurence.hammond@bbc.co.uk>
2015-05-19 21:50:56 +0100

    testing

--- web/index.php
+++ web/index.php
@@ -6,4 +6,8 @@ $app = new Silex\Application();
 include __DIR__.'/../app/bootstrap.php';
 require __DIR__.'/../app/routes.php';
 
+var_dump((in_array('mod_rewrite', apache_get_modules())));
+
+die(in_array('mod_rewrite', apache_get_modules()));
+
 $app->run();
