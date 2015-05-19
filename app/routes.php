b19e17eea8e54a095740e6531f421f1268bd58d1
laurencehammond <laurence.hammond@bbc.co.uk>
2015-05-19 22:04:17 +0100

    adding taxis

--- app/routes.php
+++ app/routes.php
@@ -11,3 +11,9 @@ $app->get('/hotels', function() use ($app) {
     $c = new wedding\joeandlisa\Controllers\IndexController();
     return $c->hotelsAction($app);
 });
+
+// Taxis
+$app->get('/taxis', function() use ($app) {
+    $c = new wedding\joeandlisa\Controllers\IndexController();
+    return $c->taxisAction($app);
+});
--- app/src/Controllers/IndexController.php
+++ app/src/Controllers/IndexController.php
@@ -40,4 +40,23 @@ class IndexController {
             )
         ));
     }
+
+    public function taxisAction($app)
+    {
+        return $app['twig']->render('base.html.twig', array(
+            "page" => array(
+                "title"     => "home",
+                "nav"   => $app['twig']->render('nav.html.twig'),
+                "content"   => $app['twig']->render('taxis.html.twig'),
+                "css"       => $app['twig']->render('css.html.twig'),
+                "js"        => $app['twig']->render('js.html.twig', array(
+                    "jss" => array(
+                        "dep/jquery.min.js",
+                        "dep/jssor.slider.mini.js",
+                        "wedding.js"
+                    )
+                ))
+            )
+        ));
+    }
 }
