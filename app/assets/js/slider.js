c7ca8924a00b4e7daf100b11984a00ff7ce5ca77
laurencehammond <laurence.hammond@bbc.co.uk>
2015-05-26 22:00:05 +0100

    fixing height proportions

--- app/assets/js/slider.js
+++ app/assets/js/slider.js
@@ -5,7 +5,19 @@
     }
 
     Slider.prototype.init = function() {
-        this.slider = new JSSlider('index-gallery', this.options());
+        if (window.innerWidth < 799) {
+            document.querySelector("#index-gallery").style.width = window.innerWidth + "px";
+            document.querySelector("#index-gallery .gallery").style.width = window.innerWidth + "px";
+
+            var height = (window.innerWidth / 1000) * 747;
+
+            document.querySelector("#index-gallery").style.height = height + "px";
+            document.querySelector("#index-gallery .gallery").style.height = height + "px";
+
+            this.slider = new JSSlider('index-gallery', this.options());
+        } else {
+            this.slider = new JSSlider('index-gallery', this.options());
+        }
     };
 
     Slider.prototype.options = function() {
